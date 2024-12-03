e67a4185a94083b05c64b06a90db3bb7209bfd37
Maurice Ambroise <maurice.ambroise79@gmail.com>
2024-12-03 22:16:30 +0100

    ajout entités(models): forum et reaction

--- emodon_main/models/forum.py
+++ emodon_main/models/forum.py
@@ -5,3 +5,29 @@
 # Author : Morice
 # ---------------------------------------------------------------------------
 
+from django.db import models
+from django.contrib.auth.models import User
+
+class Forum(models.Model):
+    MOOD_CHOICE = [
+        ('bad' , 'I feel bad'),
+        ('alone' , 'I feel lonely'),
+        ('depressed' , 'I feel depressed'),
+        ('hard' , 'Life is so hard'),
+        ('useless' , 'I feel useless'),
+        ('tired' , 'I feel tired'),
+    ]
+
+    title = models.CharField(max_length=50, choices=MOOD_CHOICE)
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forums')
+
+    def __str__(self):
+        return f"{self.title} - {self.mood}"
+
+    class Meta:
+        # for admin interface
+        verbose_name = "Forum"
+        verbose_name_plural = "Forums"
+        # setting behavior for the request
+        ordering = ['-created_at'] 
